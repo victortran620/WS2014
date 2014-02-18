ab76f94100c925b5e70082088fb9761941e6f8e2
Tyten Teegarden <tyten@tyten.com>
2014-02-18 13:50:57 -0600

    Table View Demo.

---
+++ EpisodeListDemo/EpisodeListDemo/MIZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MIZAppDelegate.h
+//  EpisodeListDemo
+//
+//  Created by Class on 2/18/14.
+//  Copyright (c) 2014 Mizzou IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MIZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EpisodeListDemo/EpisodeListDemo/View Controller/MIZEpisodeListTableViewController.h
@@ -0,0 +1,19 @@
+//
+//  MIZEpisodeListTableViewController.h
+//  EpisodeListDemo
+//
+//  Created by Class on 2/18/14.
+//  Copyright (c) 2014 Mizzou IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MIZEpisodeListTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *seasonOne;
+@property (nonatomic, strong) NSArray *seasonTwo;
+@property (nonatomic, strong) NSArray *seasonThree;
+
+@property (nonatomic, strong) NSArray *showSeasons;
+
+@end
